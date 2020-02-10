c0891722ef4da2a935c1e00cc5d8f4c79609bbae
zaholy16 <60717117+zaholy16@users.noreply.github.com>
2020-02-10 16:34:13 -0600

    Calificacion en el sistema Americano
    Muestra un numero aleatorio entre 1-10 y te dice su equivalente en el sistema Americano.

---
+++ calificacionAmericano.js
@@ -0,0 +1,19 @@
+var num=Math.floor(Math.random()*10)+1; //Numero Aleatorio entre 1-10
+
+console.log(num);
+
+if(num==10)
+    console.log("Tienes una A");
+    else if(num==9)
+        console.log("Tienes una B");
+    else if(num==8)
+        console.log("Tienes una C");
+    else if(num==7)
+        console.log("Tienes una D");
+    else if(num==6)
+        console.log("Tienes una E");
+else
+    console.log("Tienes una F");
+
+
+
