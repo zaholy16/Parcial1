a8ff02c7bb09fc16c0a5b44a16a55f5e5277855f
zaholy16 <60717117+zaholy16@users.noreply.github.com>
2020-02-21 17:29:59 -0600

    Agregue variable cuantos
    Se agrego la variable "cuantos" que determina cuantos términos quiere que recorra la serie.

--- eExpo.js
+++ eExpo.js
@@ -1,8 +1,8 @@
 function SerieMaclaurin(s)
 {
-    var num=1, factorialdeno=1;
+    var num=1, factorialdeno=1, cuantos=10;
     var op2=0, suma=1;
-    for(var i=1; i<=s; i++)
+    for(var i=1; i<=cuantos; i++)
     {    
         factorialdeno*=i;
         num=s**i;
